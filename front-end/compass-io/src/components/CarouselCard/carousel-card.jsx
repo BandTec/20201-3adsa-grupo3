03cf49a5fd0813b738a55dd0b14485aaa0dd420d
brunochj <brunochang95@gmail.com>
2020-11-16 17:31:48 -0300

    (front): cria telas e componentes para a tela de vaga específica

---
+++ front-end/compass-io/src/components/CarouselCard/carousel-card.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Icon, InlineIcon } from '@iconify/react';
+import locationIcon from '@iconify/icons-carbon/location';
+import calendarOutlined from '@iconify/icons-ant-design/calendar-outlined';
+import Fab from '@material-ui/core/Fab';
+import NextIcon from '@material-ui/icons/KeyboardArrowRight';
+import nextOutline from '@iconify/icons-carbon/next-outline';
+import './carousel-card.css';
+
+function CarouselCard(props){
+    return(
+        <div className="carouselCard">
+            <div className="cardImg">
+
+            </div>
+            <div className="cardContent">
+                <div id="contentNameOng">
+                    Por {props.nameOng}
+                </div> 
+                <h3 id="contentTitle">
+                    {props.title}
+                </h3>    
+                <div id="contentDescription">
+                    {props.description}
+                </div>
+                <div id="cardDescription">
+                    <div>
+                        <div id="contentLocation">
+                            <Icon icon={locationIcon} style={{fontSize: '32px'}} />
+                            <div id="contentLocationText">
+                                {props.location}
+                            </div>
+                        </div> 
+                        <div id="contentSchedule">
+                            <Icon icon={calendarOutlined} style={{fontSize: '32px'}} />
+                            <div id="contentScheduleText">
+                                {props.schedule}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="goNextBtn">
+                        <a href="www.google.com">
+                            <Icon icon={nextOutline} style={{fontSize: '42px'}} />
+                        </a>
+                    </div>   
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default CarouselCard;
---
+++ front-end/compass-io/src/components/VacancyHeader/vacancy-header.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './vacancy-header.css';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { Icon, InlineIcon } from '@iconify/react';
+import heartOutlined from '@iconify/icons-ant-design/heart-outlined';
+
+const useStyles = makeStyles({
+    btnFavorite:{
+        backgroundColor: '#FFB219',
+        color: '#fff',
+        width: '10rem',
+        height: '3rem',
+        fontWeight: 700,
+        border: '1px solid #000',
+    },
+    btnApply:{
+        backgroundColor: '#1975FF',
+        color: '#fff',
+        width: '10rem',
+        height: '3rem',
+        fontWeight: 700,
+        border: '1px solid #000',
+        marginLeft: '2rem'
+    }
+});
+
+export default function VacancyHeader(props) {
+    const classes = useStyles();
+    return(
+        <div className="vacancyHeader">
+            <div id="imgVacancy">
+            </div>
+            <span id="headerText">
+                <div id="headerTitle">{ props.title }</div>
+                <div id="headerBtn">
+                    <Button className={classes.btnFavorite}>
+                        <Icon icon={heartOutlined} style={{color: '#eeeeee', fontSize: '2rem'}} />Favoritar
+                    </Button>
+                    <Button className={classes.btnApply}>Candidatar-se</Button>
+                </div>
+            </span>
+        </div>
+    );
+}
