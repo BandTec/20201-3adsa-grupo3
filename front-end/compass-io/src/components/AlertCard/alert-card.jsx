347c9e30d5f7e4a9dde0fb81a979629ab8a8351f
daniel-fajan <daniel.anselmi@bandtec.com.br>
2020-12-06 21:51:52 -0300

    (front-end) [quebrado] Atualiza componente de Alert

--- front-end/compass-io/src/components/AlertCard/alert-card.jsx
+++ front-end/compass-io/src/components/AlertCard/alert-card.jsx
@@ -1,61 +1,32 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React from 'react'
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
-import { makeStyles } from '@material-ui/core/styles';
-
-import './alert-card.css';
 
 function Alert(props) {
-  return (
-      <div>
-        <MuiAlert elevation={6} variant="filled" {...props} ><label>{props.menssage}</label></MuiAlert>
-      </div>
-  );
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
+export default function AlertCard(props) {
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '50%',
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
+    const [state, setState] = React.useState({
+      open: true
+    });
 
-export default function CustomizedSnackbars(props) {
-    /*{Textos para status do alerta: "success", "error", "warning", "info"}*/
+    const open = state.open;
 
-  const classes = useStyles();
-  const [state, setState] = React.useState({
-    open: false,
-    vertical: 'top',
-    horizontal: 'center',
-  });
+    const fecharAlerta = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setState({open: false})
+    };
 
-  const { vertical, horizontal, open } = state;
-  
-   const handleClick = (newState) => () => {
-    setState({ open: true, ...newState });
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    setState(false);
-  };    
-
-  return (
-    <div className={classes.root}>
-      <Button id="buttonCSS" variant="contained" color="inherit" onClick={handleClick({vertical: "top", horizontal: "right"})}>
-        Cadastrar
-      </Button>
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} anchorOrigin={{vertical, horizontal}}>
-        <Alert onClose={handleClose} severity={props.statusAlert} menssage="Só sucesso no alerta!"/>
-      </Snackbar>
-    </div>
-  );
+    return (
+        <Snackbar open={open} autoHideDuration={6000} onClose={fecharAlerta}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }} >
+            <Alert onClose={fecharAlerta} severity={props.severity}>
+                {props.message}
+            </Alert>
+        </Snackbar>
+    )
 }
