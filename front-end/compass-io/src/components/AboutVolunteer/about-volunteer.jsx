315703e665e4f3dfd9ce39403f4c2c8d0111aaf2
Iago Roani de lima <iago.lima@bandtec.com.br>
2020-12-10 02:25:06 -0300

    (front-end)Ajuste no componente AboutVolunteer

--- front-end/compass-io/src/components/AboutVolunteer/about-volunteer.jsx
+++ front-end/compass-io/src/components/AboutVolunteer/about-volunteer.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import InputFile from '../InputFile/input-file';
+import { makeStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
 
 import './about-volunteer.css';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      maxWidth: '36ch',
+      backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+      display: 'inline',
+    },
+  }));
+
 function AboutVolunteer(props) {
+    const classes = useStyles();
+
     return (
        <section className="width-100pg flex ">
 
@@ -26,20 +44,120 @@ function AboutVolunteer(props) {
                </div>
            </div>
 
-           {/* <div className="vagasBox">
-               <div>
-                   <span>Vagas acessadas</span>
-               </div>
-               <a href="https://www.google.com/"><img width="60" height="70" src={props.imgVolunteer}></img></a>
-               <a href="https://www.google.com/"><img width="60" height="70" src={props.imgVolunteer}></img></a>
-               <a href="https://www.google.com/"><img width="60" height="70" src={props.imgVolunteer}></img></a>
-               <a href="https://www.google.com/"><img width="60" height="70" src={props.imgVolunteer}></img></a>
-               <a href="https://www.google.com/"><img width="60" height="70" src={props.imgVolunteer}></img></a>
-               <a href="https://www.google.com/"><img width="60" height="70" src={props.imgVolunteer}></img></a>
-               <a href="https://www.google.com/"><img width="60" height="70" src={props.imgVolunteer}></img></a>
-               <a href="https://www.google.com/"><img width="60" height="70" src={props.imgVolunteer}></img></a>
-               <a href="https://www.google.com/"><img width="60" height="70" src={props.imgVolunteer}></img></a>
-           </div> */}
+           <div className="vagasBox">
+                <div className="titleBox">
+                   <span><u>Vagas acessadas</u></span>
+                </div>
+                <div className="filaDeVagas">
+                <scroll-container className={classes.root}>
+                    <scroll-page alignItems="flex-start">
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar alt="Vacancy Ong" src={props.imgVagaOng}/>
+                                </ListItemAvatar>
+                                <ListItemText id="listItem"
+                                    primary="Titulo da Vaga"
+                                    secondary={
+                                        <React.Fragment>
+                                        {"Descrição vai ficar aqui, um props para trocar conforme a vaga"}
+                                        </React.Fragment>
+                                    }
+                                />
+                            </ListItem>
+                    </scroll-page>
+                    <scroll-page alignItems="flex-start">
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                                </ListItemAvatar>
+                                <ListItemText id="listItem"
+                                    primary="Titulo da Vaga"
+                                    secondary={
+                                    <React.Fragment>
+                                    {"Descrição vai ficar aqui, um props para trocar conforme a vaga"}
+                                    </React.Fragment>
+                                }
+                                />
+                            </ListItem>    
+                    </scroll-page>
+                    <scroll-page alignItems="flex-start">
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                                </ListItemAvatar>
+                                <ListItemText id="listItem"
+                                    primary="Titulo da Vaga"
+                                    secondary={
+                                        <React.Fragment>
+                                        {"Descrição vai ficar aqui, um props para trocar conforme a vaga"}
+                                        </React.Fragment>
+                                }
+                                />
+                            </ListItem>
+                    </scroll-page>
+                    <scroll-page alignItems="flex-start">
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar alt="Vacancy Ong" src={props.imgVagaOng}/>
+                                </ListItemAvatar>
+                                <ListItemText id="listItem"
+                                    primary="Titulo da Vaga"
+                                    secondary={
+                                        <React.Fragment>
+                                        {"Descrição vai ficar aqui, um props para trocar conforme a vaga"}
+                                        </React.Fragment>
+                                    }
+                                />
+                            </ListItem>
+                    </scroll-page>
+                    <scroll-page alignItems="flex-start">
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                                </ListItemAvatar>
+                                <ListItemText id="listItem"
+                                    primary="Titulo da Vaga"
+                                    secondary={
+                                    <React.Fragment>
+                                    {"Descrição vai ficar aqui, um props para trocar conforme a vaga"}
+                                    </React.Fragment>
+                                }
+                                />
+                            </ListItem>    
+                    </scroll-page>
+                    <scroll-page alignItems="flex-start">
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar alt="Vacancy Ong" src={props.imgVagaOng}/>
+                                </ListItemAvatar>
+                                <ListItemText id="listItem"
+                                    primary="Titulo da Vaga"
+                                    secondary={
+                                        <React.Fragment>
+                                        {"Descrição vai ficar aqui, um props para trocar conforme a vaga"}
+                                        </React.Fragment>
+                                    }
+                                />
+                            </ListItem>
+                    </scroll-page>
+                    <scroll-page alignItems="flex-start">
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                                </ListItemAvatar>
+                                <ListItemText id="listItem"
+                                    primary="Titulo da Vaga"
+                                    secondary={
+                                    <React.Fragment>
+                                    {"Descrição vai ficar aqui, um props para trocar conforme a vaga"}
+                                    </React.Fragment>
+                                }
+                                />
+                            </ListItem>    
+                    </scroll-page>
+                </scroll-container>
+                </div>
+           </div>
        </section>
     );
 }
