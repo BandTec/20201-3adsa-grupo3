3eb5ade935771abee5a3beaaaeec30255cd761b8
daniel-fajan <daniel.anselmi@bandtec.com.br>
2020-12-11 23:26:57 -0300

    (front-end) Ajusta foto do voluntario

--- front-end/compass-io/src/pages/ProfileVolunteer/profile-volunteer.jsx
+++ front-end/compass-io/src/pages/ProfileVolunteer/profile-volunteer.jsx
@@ -52,36 +52,47 @@ async function getFoto() {
   imgVolunteer.src = "data:image/png;base64," + fotoResponse.data;
 }
 
-export default function ProfileVolunteer() {
-  return (
-    <div classname="containerProfileVolunteer">
-      <div id="alertArea"></div>
-      <AboutVolunteer imgId="imgVolunteer" editImgVolunteer={trocarFoto} className="mg-b-16" nameVolunteer="Iago Roani de Lima" ageVolunteer="21 anos" professionVolunteer="Automação"
-      schoolVolunteer="Cursando Superior" liveInVolunteer="Suzano,SP,Brasil"></AboutVolunteer>
-      <div className="">
-        <h1>Sobre mim</h1>
+export default class ProfileVolunteer extends React.Component {
+  
+  state = {
+
+  }
+
+  componentDidMount() {
+    getFoto()
+  }
+
+  render() {
+    return (
+      <div classname="containerProfileVolunteer">
+        <div id="alertArea"></div>
+        <AboutVolunteer imgId="imgVolunteer" editImgVolunteer={trocarFoto} className="mg-b-16" nameVolunteer="Iago Roani de Lima" ageVolunteer="21 anos" professionVolunteer="Automação"
+        schoolVolunteer="Cursando Superior" liveInVolunteer="Suzano,SP,Brasil"></AboutVolunteer>
+        <div className="">
+          <h1>Sobre mim</h1>
+        </div>
+        {/* <span className><h1><u><b>Sobre Mim</b></u></h1></span> */}
+        <div classname="descriptionVolunteer">
+        <CommentBox/>
+        </div>
+        <div>
+          <h1><u>Interesses</u></h1>
+          <CarouselInterests/>
+        </div>
+        <div>
+          <h1><u>Competências</u></h1>
+          <CarouselSkills/>
+        </div>
+        <div classname="ratingBox">
+        <Rating isVolunteerProfile 
+              imgVolunteer={ImgVolunteer}
+              vacancyTitle="Marcenaria para construção de móveis" 
+              infoVacancy="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Iusto asperiores excepturi cum dolores ipsam delectus minima nesciunt dignissimos, voluptates, 
+                        accusantium cupiditate incidunt laboriosam aspernatur. P
+                        laceat ut maxime facilis molestias pariatur!"/>
+        </div>
       </div>
-      {/* <span className><h1><u><b>Sobre Mim</b></u></h1></span> */}
-      <div classname="descriptionVolunteer">
-      <CommentBox/>
-      </div>
-      <div>
-        <h1><u>Interesses</u></h1>
-        <CarouselInterests/>
-      </div>
-      <div>
-        <h1><u>Competências</u></h1>
-        <CarouselSkills/>
-      </div>
-      <div classname="ratingBox">
-      <Rating isVolunteerProfile 
-            imgVolunteer={ImgVolunteer}
-            vacancyTitle="Marcenaria para construção de móveis" 
-            infoVacancy="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                      Iusto asperiores excepturi cum dolores ipsam delectus minima nesciunt dignissimos, voluptates, 
-                      accusantium cupiditate incidunt laboriosam aspernatur. P
-                      laceat ut maxime facilis molestias pariatur!"/>
-      </div>
-    </div>
-  );
+    );
+  }
 };
