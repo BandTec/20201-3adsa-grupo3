e584bc87c573528edf0bfece7ade11299efa0773
daniel-fajan <daniel.anselmi@bandtec.com.br>
2021-03-02 23:49:12 -0300

    (documentacao) Atualiza script da tabela 'publicacao'

--- documentacao/banco-de-dados/scripts/ScriptTabelaPublicacao26012021.sql
+++ documentacao/banco-de-dados/scripts/ScriptTabelaPublicacao26012021.sql
@@ -2,7 +2,7 @@
 CREATE TABLE publicacao (
 	id INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
 	descricao VARCHAR(200),
-	imagem VARCHAR(MAX),
+	imagem VARCHAR(150),
 	curtidas INT,
 	id_usuario INT NOT NULL,
 	tipo_usuario VARCHAR(2) NOT NULL
